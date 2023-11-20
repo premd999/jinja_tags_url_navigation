33defe9021d8f4b2f4db7fac0f1cb28c2a4aceb1
premd999 <premdpk999@gmail.com>
2023-11-20 22:26:54 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def rajesh(request):
+    return render(request,'rajesh.html')
+
+def srujana(request):
+    return render(request, 'srujana.html')
+
